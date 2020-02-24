6a99fe87baffa3b4fb71ad8354aadd67af4edce5
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 02:08:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ assigned_room
 end
 
 def printer(attendees)
-counter = 1
+counter = -1
 while counter <= batch_badge_creator(attendees).length
  puts batch_badge_creator(attendees)[counter]
  # puts assign_rooms(attendees)[counter]
