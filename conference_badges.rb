27828d2bac09500c5f2422ca0cf8d5dbc2721254
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 03:09:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  greeting = []
+  attendees.each do |names|
+    sentence = "Hello, my name is #{names}."
+  greeting.push(sentence)
+  end
+  greeting
+end
+
+def assign_rooms(name)
+  assigned_room = []
+  count = 1
+  name.each do |name|
+  attendees = "Hello, #{name}! You'll be assigned to room #{count}!"
+  assigned_room.push(attendees)
+  count += 1
+end
+assigned_room
+end
+
+def printer(attendees)
+  counter = 0
+    while  counter <= attendees.length - 1
+     puts batch_badge_creator(attendees)[counter]
+     puts assign_rooms(attendees)[counter]
+     counter += 1
+     end
+end
